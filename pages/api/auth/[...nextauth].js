52fb6bc586469b745ada8387fc1419d50ad4c57a
eunhye <masimel33@gmail.com>
2024-11-07 17:09:51 +0900

    study

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+import { connectDB } from "@/util/database";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
@@ -11,6 +13,7 @@ export const authOptions = {
         // ...add more providers here
     ],
     secret: "qwer1234",
+    adapter: MongoDBAdapter(connectDB),
 };
 
 export default NextAuth(authOptions);
